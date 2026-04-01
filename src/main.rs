a57336c8bd2d053e28dbe6d00c350f13fa3a6526
hrdpsingh <harshdeep.s.rakkar@gmail.com>
2026-04-01 13:31:38 +0530

    Added hardware column and additional fields.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use iced::{
-    Application, Command, Element, Settings, Theme, executor,
-    widget::{column, text},
+    Application, Command, Element, Font, Settings, Theme, executor,
+    font::Weight,
+    widget::{column, row, text},
 };
 use nix::sys::utsname::uname;
+use std::{env, fs, path::Path};
+use sysinfo::{Disks, System};
 
 fn main() -> iced::Result {
     ByteScope::run(Settings::default())
@@ -11,6 +14,12 @@ fn main() -> iced::Result {
 struct ByteScope {
     hostname: String,
     kernel: String,
+    os: String,
+    display_server: String,
+    ram: u64,
+    storage: u64,
+    cpu: String,
+    product_name: String,
 }
 
 #[derive(Debug, Clone)]
@@ -24,16 +33,59 @@ impl Application for ByteScope {
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
         let hostname = match hostname::get() {
-            Ok(name) => name.to_string_lossy().to_string(),
+            Ok(val) => val.to_string_lossy().to_string(),
             Err(_) => "Unavailable".to_string(),
         };
 
         let kernel = match uname() {
-            Ok(info) => info.release().to_string_lossy().to_string(),
+            Ok(val) => val.release().to_string_lossy().to_string(),
             Err(_) => "Unavailable".to_string(),
         };
 
-        (Self { hostname, kernel }, Command::none())
+        let os = os_info::get().to_string();
+
+        let display_server = match env::var("XDG_SESSION_TYPE") {
+            Ok(val) => val,
+            Err(_) => "Unavailable".to_string(),
+        };
+
+        let mut sys = System::new();
+        sys.refresh_memory();
+        let ram = sys.total_memory();
+
+        let storage = Disks::new_with_refreshed_list()
+            .list()
+            .iter()
+            .find(|disk| disk.mount_point() == Path::new("/"))
+            .map(|disk| disk.total_space())
+            .expect("Unavailable");
+
+        let cpuinfo = fs::read_to_string("/proc/cpuinfo").unwrap();
+        let mut cpu = "Unavailable".to_string();
+        for line in cpuinfo.lines() {
+            if line.starts_with("model name")
+                && let Some((_, name)) = line.split_once(':')
+            {
+                cpu = name.trim().to_string();
+            }
+        }
+
+        let product_name = fs::read_to_string("/sys/devices/virtual/dmi/id/product_name")
+            .unwrap_or_else(|_| "Unknown".into());
+
+        (
+            Self {
+                hostname,
+                kernel,
+                os,
+                display_server,
+                ram,
+                storage,
+                cpu,
+                product_name,
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -45,12 +97,38 @@ impl Application for ByteScope {
     }
 
     fn view(&self) -> Element<'_, Message> {
-        column![
+        let software = column![
+            text("Software").font(Font {
+                weight: Weight::Bold,
+                ..Font::default()
+            }),
             text(format!("Hostname: {}", self.hostname)),
             text(format!("Kernel: {}", self.kernel)),
+            text(format!("OS: {}", self.os)),
+            text(format!("Display Server: {}", self.display_server)),
         ]
         .padding(20)
-        .spacing(10)
-        .into()
+        .spacing(10);
+
+        let hardware = column![
+            text("Hardware").font(Font {
+                weight: Weight::Bold,
+                ..Font::default()
+            }),
+            text(format!(
+                "Memory: {:.2} GB",
+                self.ram as f64 / (1024.0 * 1024.0 * 1024.0)
+            )),
+            text(format!(
+                "Storage: {:.2} GB",
+                self.storage as f64 / (1024.0 * 1024.0 * 1024.0)
+            )),
+            text(format!("CPU: {}", self.cpu)),
+            text(format!("Product Name: {}", self.product_name)),
+        ]
+        .padding(20)
+        .spacing(10);
+
+        row![software, hardware,].spacing(20).padding(20).into()
     }
 }
